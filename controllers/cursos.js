d54d71c30ca572b12a9180487d568f481db8243c
Camilo Rodriguez <118146012+Camilo13056@users.noreply.github.com>
2024-03-13 14:08:12 -0500

    Agregando y probando la funcionalidad para Eliminar (Inactivar) cursos. EndPoint de tipo DELETE.

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -32,6 +32,24 @@ ruta.post('/', (req, res) => {
     })
 });
 
+// Función asíncrona para inactivar cursos
+async function desactivarCurso(id){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            estado: false
+        }
+    }, {new: true});
+    return curso;
+}
 
+// Endpoint de tipo DELETE para el recurso CURSOS
+ruta.delete('/:id', (req, res) => {
+    let resultado = desactivarCurso(req.params.id);
+    resultado.then(curso => {
+        res.json(curso);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
 
 module.exports = ruta;
